1a7ce04332ee4864cb73435c801220e2ee08fa55
Noam Alffasy <noamalffasy@Noams-iMac.local>
2018-05-29 19:26:57 +0300

    Removed the old server as it's no longer needed

--- server/db/models/index.ts
+++
@@ -1,8 +0,0 @@
-import * as Sequelize from "sequelize";
-
-export * from "./User";
-export * from "./Session";
-export * from "./Article";
-export * from "./Quote";
-export * from "./Comment";
-export * from "./ResetSession";
