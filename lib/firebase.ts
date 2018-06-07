45f20c3cda85b3a8569934b6f79cd3b01ad006d9
Noam Alffasy <noamalffasy@Noams-iMac.local>
2018-06-07 22:53:56 +0300

    Changed domain to oyah.xyz on firebase auth callback

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,7 +2,7 @@ import * as firebase from "firebase";
 
 const config = {
   apiKey: "AIzaSyD3vyx6XFFztE0qlh9gljA83BWK7rAY0Tk",
-  authDomain: "oyah-200816.firebaseapp.com",
+  authDomain: "oyah.xyz",
   databaseURL: "https://oyah-200816.firebaseio.com",
   projectId: "oyah-200816",
   storageBucket: "oyah-200816.appspot.com",
