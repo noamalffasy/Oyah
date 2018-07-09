13a8f77678dbad22550d994d77f65de574c12526
Noam Alffasy <noamalffasy@Noams-iMac.local>
2018-07-09 22:52:49 +0300

    Removed the firebase models since they are not used

--- functions/src/db/models_firebase/index.ts
+++
@@ -1,6 +0,0 @@
-import * as User from "./User";
-import * as Article from "./Article";
-import * as Quote from "./Quote";
-import * as Comment from "./Comment";
-
-export { User, Article, Quote, Comment };
