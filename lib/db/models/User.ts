7000585059fee049542a4eeeca5edb3c206b82d9
Noam Alffasy <noamalffasy@Noams-iMac.local>
2018-07-09 22:51:41 +0300

    Added all the db models to the browser

---
+++ lib/db/models/User.ts
@@ -0,0 +1,21 @@
+import { db } from "../../firebase";
+import Model from "./Model";
+
+const users = db.ref("/users");
+
+export interface Interface {
+  id: string;
+  nametag?: string;
+  name?: string;
+  email?: string;
+  image?: string;
+  small_image?: string;
+  bio?: string;
+  mains?: string;
+  reddit?: string;
+  twitter?: string;
+  providerId?: string;
+  is_team?: boolean;
+}
+
+export default new Model(users);
