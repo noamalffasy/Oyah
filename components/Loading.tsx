0deba24291e5bb396ba5a7b8b8d9affe7a7a9e9f
Noam Alffasy <noamalffasy@Noams-iMac.local>
2018-08-07 16:28:45 +0300

    Fixed formatting on Loading

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,40 +2,40 @@ import * as React from "react";
 import { Component } from "react";
 
 class Loading extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className="loading" />
-                <style jsx>{`
-                    @keyframes spinAround {
-                      0% {
-                        -webkit-transform: rotate(0);
-                        transform: rotate(0);
-                      }
-                      100% {
-                        -webkit-transform: rotate(359deg);
-                        transform: rotate(359deg);
-                      }
-                    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="loading" />
+        <style jsx>{`
+          @keyframes spinAround {
+            0% {
+              -webkit-transform: rotate(0);
+              transform: rotate(0);
+            }
+            100% {
+              -webkit-transform: rotate(359deg);
+              transform: rotate(359deg);
+            }
+          }
 
-                    .loading {
-                        content: "";
-                        display: block;
-                        position: relative;
-                        height: 1.5rem;
-                        width: 1.5rem;
-                        margin: 0 auto;
-                        border: 2px solid #dbdbdb;  
-                        border-color: #cc0000;
-                        border-radius: 290486px;
-                        border-right-color: transparent;
-                        border-top-color: transparent;
-                        animation: spinAround .5s infinite linear;
-                    }
-                `}</style>
-            </React.Fragment>
-        );
-    }
+          .loading {
+            content: "";
+            display: block;
+            position: relative;
+            height: 1.5rem;
+            width: 1.5rem;
+            margin: 0 auto;
+            border: 2px solid #dbdbdb;
+            border-color: #cc0000;
+            border-radius: 290486px;
+            border-right-color: transparent;
+            border-top-color: transparent;
+            animation: spinAround 0.5s infinite linear;
+          }
+        `}</style>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Loading;
